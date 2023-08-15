c2930c3fd993e9536ea50b1553fbaa478a278818
Christoph Bless <bitbucket@cbless.de>
2023-08-15 21:48:15 +0200

    #28 WSUS Excel added

--- systemdb/core/export/excel/hosts.py
+++ systemdb/core/export/excel/hosts.py
@@ -136,3 +136,47 @@ def generate_hosts_excel_brief(hosts=[]):
     # Rewind the buffer.
     output.seek(0)
     return output
+
+
+
+def generate_wsus(hosts=[]):
+    output = BytesIO()
+    workbook = xlsxwriter.Workbook(output, {"in_memory": True})
+    worksheet = workbook.add_worksheet()
+
+    rows = []
+
+    for h in hosts:
+        tmp = [h.Hostname, h.SystemGroup, h.Location, h.Domain, h.OSName, h.WUServer, h.LastUpdate]
+        rows.append(tmp)
+
+
+    header_data = ["Hostname", "Location", "Systemgroup", "Domain", "OSName", "WSUS Server", "Last Update"]
+
+    wrap_format = workbook.add_format({'text_wrap': True})
+    header_format = workbook.add_format({'bold': True,
+                                         'bottom': 2,
+                                         'bg_color': '#CCCCCC'})
+
+    for col_num, data in enumerate(header_data):
+        worksheet.write(0, col_num, data, header_format)
+
+    # Start from the first cell. Rows and columns are zero indexed.
+    row = 1
+    col = 0
+    # Iterate over the data and write it out row by row.
+    for host in rows:
+        for c in host:
+            worksheet.write(row, col, str(c))
+            col += 1
+        col = 0
+        row += 1
+
+    worksheet.autofilter("A1:H1")
+    worksheet.autofit()
+    # Close the workbook before streaming the data.
+    workbook.close()
+
+    # Rewind the buffer.
+    output.seek(0)
+    return output
--- systemdb/webapp/sysinfo/reports/wsus.py
+++ systemdb/webapp/sysinfo/reports/wsus.py
@@ -4,6 +4,7 @@ from flask_login import login_required
 from systemdb.webapp.sysinfo import sysinfo_bp
 from systemdb.core.export.excel.hosts import generate_hosts_excel
 from systemdb.core.export.excel.hosts import generate_hosts_excel_brief
+from systemdb.core.export.excel.hosts import generate_wsus
 from systemdb.core.models.sysinfo import Host
 from systemdb.core.reports import ReportInfo
 
@@ -12,31 +13,40 @@ from systemdb.core.reports import ReportInfo
 ####################################################################
 @sysinfo_bp.route('/report/wsus-http/excel/full', methods=['GET'])
 @login_required
-def hosts_report_wsus_http_excel_full():
+def report_wsus_http_excel_hosts_full():
     hosts = Host.query.filter(Host.WUServer.like('http://%'))
     output = generate_hosts_excel(hosts)
     return Response(output, mimetype="text/xlsx",
-                    headers={"Content-disposition": "attachment; filename=hosts-with-smbv1-full.xlsx",
+                    headers={"Content-disposition": "attachment; filename=hosts-with-wsus-via-http.xlsx",
                              "Content-type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" })
 
 
 @sysinfo_bp.route('/report/wsus-http/excel/brief', methods=['GET'])
 @login_required
-def hosts_report_wsus_http_excel_brief():
+def report_wsus_http_excel_hosts_brief():
     hosts = Host.query.filter(Host.WUServer.like('http://%'))
     output = generate_hosts_excel_brief(hosts)
     return Response(output, mimetype="text/xlsx",
-                    headers={"Content-disposition": "attachment; filename=hosts-with-smbv1-brief.xlsx",
+                    headers={"Content-disposition": "attachment; filename=hosts-with-wsus-via-http.xlsx",
                              "Content-type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" })
 
 
-@sysinfo_bp.route('/report/wsus-http', methods=['GET'])
+
+@sysinfo_bp.route('/report/wsus-http/excel/wsus', methods=['GET'])
 @login_required
-def hosts_report_wsus_http():
+def report_wsus_http_excel_wsus():
     hosts = Host.query.filter(Host.WUServer.like('http://%'))
-    return render_template('host_list.html', hosts=hosts,
-                           download_brief_url=url_for("sysinfo.hosts_report_wsus_http_excel_brief"),
-                           download_url=url_for("sysinfo.hosts_report_wsus_http_excel_full"))
+    output = generate_wsus(hosts)
+    return Response(output, mimetype="text/xlsx",
+                    headers={"Content-disposition": "attachment; filename=wsus-via-http.xlsx",
+                             "Content-type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" })
+
+
+@sysinfo_bp.route('/report/wsus-http/', methods=['GET'])
+@login_required
+def report_wsus_http():
+    hosts = Host.query.filter(Host.WUServer.like('http://%'))
+    return render_template('report_wsus_list.html', hosts=hosts)
 
 
 
@@ -49,5 +59,5 @@ class ReportWSUSHttp(ReportInfo):
             category="Systemhardening",
             tags=["Systemhardening", "WSUS", "Cleartext protocol"],
             description='Report all hosts where the WSUS server is configured to be reached via http',
-            views=[("view", url_for("sysinfo.hosts_report_wsus_http"))]
+            views=[("view", url_for("sysinfo.report_wsus_http"))]
         )
