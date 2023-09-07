f1fa197b638b76a82bfd62b227e985e9a9ce8032
Christoph Bless <bitbucket@cbless.de>
2023-09-08 00:31:19 +0200

    #7 API-Endpoint for list all users by-domain added

--- systemdb/webapi/ad/views/users.py
+++ systemdb/webapi/ad/views/users.py
@@ -6,7 +6,8 @@ from systemdb.core.models.activedirectory import ADUser
 from systemdb.webapi.ad import bp
 from systemdb.webapi.extentions import auth
 
-from systemdb.webapi.ad.schemas.responses.domain import ADUserSchema
+from systemdb.webapi.ad.schemas.responses.domain import ADUserSchema, ADDomain
+from systemdb.webapi.ad.schemas.arguments.users import UserByDomainSearchSchema
 
 
 @bp.get("/domain/<int:domain_id>/users/")
@@ -34,3 +35,28 @@ def get_domain_user_list(domain_id):
         security='ApiKeyAuth')
 def get_domain_user_list_all():
     return ADUser.query.all()
+
+
+
+@bp.post("/users/by-domain/")
+@bp.auth_required(auth)
+@bp.input(schema=UserByDomainSearchSchema, location="json")
+@bp.output(status_code=HTTPStatus.OK,
+           schema=ADUserSchema(many=True),
+           description="List of users in the domain. If no users are assigned to the domain an empty list is returned.")
+@bp.doc(description="Returns a list of all domain users for the domain specified in parameters.",
+        summary="Find all domain users for the domain specified in parameters",
+        security='ApiKeyAuth'
+        )
+def post_domain_user_by_domain(search_data):
+    user_filter_list = []
+    domain_filter_list = []
+    if "id" in search_data:
+        user_filter_list.append(ADUser.Domain_id == search_data["id"])
+    if "Domainname" in search_data:
+        domain_filter_list.append(ADDomain.Name.ilike( "%"+search_data["Domainname"]+"%"))
+    if "NETBIOS" in search_data:
+        domain_filter_list.append(ADDomain.NetBIOSName.ilike( "%"+search_data["NETBIOS"]+"%"))
+
+    return ADUser.query.filter(*user_filter_list).join(ADDomain).filter().all()
+
