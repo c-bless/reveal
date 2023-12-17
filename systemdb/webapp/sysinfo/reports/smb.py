9249a16b54c0c6a50d17e651bc2ec96377287cda
Christoph Bless <github@cbless.de>
2023-12-17 02:36:02 +0100

    #56 changed download buttons for SMBv1Report to POST

--- systemdb/webapp/sysinfo/reports/smb.py
+++ systemdb/webapp/sysinfo/reports/smb.py
@@ -1,42 +1,62 @@
-from flask import render_template, Response, url_for
+from flask import render_template, Response, url_for, request
 from flask_login import login_required
+from sqlalchemy import and_
 
 from systemdb.webapp.sysinfo import sysinfo_bp
 from systemdb.core.export.excel.hosts import generate_hosts_excel
 from systemdb.core.export.excel.hosts import generate_hosts_excel_brief
 from systemdb.core.models.sysinfo import Host
 from systemdb.core.reports import ReportInfo
+from systemdb.webapp.sysinfo.forms.report.SMBv1Report import SMBv1ReportForm
 
 ####################################################################
 # Hosts with enabled SMBv1
 ####################################################################
-@sysinfo_bp.route('/report/smbv1/excel/full', methods=['GET'])
-@login_required
-def hosts_report_smbv1_excel_full():
-    hosts = Host.query.filter(Host.SMBv1Enabled == True).all()
-    output = generate_hosts_excel(hosts)
-    return Response(output, mimetype="text/xlsx",
-                    headers={"Content-disposition": "attachment; filename=hosts-with-smbv1-full.xlsx",
-                             "Content-type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" })
-
-
-@sysinfo_bp.route('/report/smbv1/excel/brief', methods=['GET'])
-@login_required
-def hosts_report_smbv1_excel_brief():
-    hosts = Host.query.filter(Host.SMBv1Enabled == True).all()
-    output = generate_hosts_excel_brief(hosts)
-    return Response(output, mimetype="text/xlsx",
-                    headers={"Content-disposition": "attachment; filename=hosts-with-smbv1-brief.xlsx",
-                             "Content-type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" })
-
-
-@sysinfo_bp.route('/report/smbv1', methods=['GET'])
+@sysinfo_bp.route('/report/smbv1', methods=['GET', 'POST'])
 @login_required
 def hosts_report_smbv1():
-    hosts = Host.query.filter(Host.SMBv1Enabled == True).all()
-    return render_template('sysinfo/host/host_list.html', hosts=hosts,
-                           download_brief_url=url_for("sysinfo.hosts_report_smbv1_excel_brief"),
-                           download_url=url_for("sysinfo.hosts_report_smbv1_excel_full"))
+    form = SMBv1ReportForm()
+
+    filter = []
+    filter.append(Host.SMBv1Enabled == True)
+
+    if request.method == 'POST':
+
+        if form.validate_on_submit():
+            systemgroup = form.SystemGroup.data
+            location = form.Location.data
+
+            invertSystemgroup = form.InvertSystemGroup.data
+            invertLocation = form.InvertLocation.data
+
+            if len(systemgroup) > 0:
+                if not invertSystemgroup:
+                    filter.append(Host.SystemGroup.ilike("%" + systemgroup + "%"))
+                else:
+                    filter.append(Host.SystemGroup.notilike("%" + systemgroup + "%"))
+            if len(location) > 0:
+                if not invertLocation:
+                    filter.append(Host.Location.ilike("%" + location + "%"))
+                else:
+                    filter.append(Host.Location.notilike("%" + location + "%"))
+
+            hosts = Host.query.filter(and_(*filter)).all()
+
+            if 'brief' in request.form:
+                output = generate_hosts_excel_brief(hosts)
+                return Response(output, mimetype="text/xlsx",
+                                headers={"Content-disposition": "attachment; filename=hosts-with-smbv1-brief.xlsx",
+                                         "Content-type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"})
+            if 'full' in request.form:
+                output = generate_hosts_excel(hosts)
+                return Response(output, mimetype="text/xlsx",
+                                headers={"Content-disposition": "attachment; filename=hosts-with-smbv1-full.xlsx",
+                                         "Content-type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"})
+
+    else:
+        hosts = Host.query.filter(and_(*filter)).all()
+    return render_template('sysinfo/reports/host_report_list.html', hosts=hosts, form=form)
+
 
 
 class ReportSMBv1(ReportInfo):
