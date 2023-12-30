da73636a20a5785d811632527faa7c6ebf592b6a
Christoph Bless <github@cbless.de>
2023-12-31 00:33:54 +0100

    #63 label field added as search field added for RegistryCheck report

--- reveal/webapp/sysinfo/reports/hardening.py
+++ reveal/webapp/sysinfo/reports/hardening.py
@@ -32,9 +32,11 @@ def report_hotkeys_enabled():
             systemgroup = form.SystemGroup.data
             location = form.Location.data
             selectedTemplate = form.TemplateFile.data
+            label = form.Label.data
 
             invertSystemgroup = form.InvertSystemGroup.data
             invertLocation = form.InvertLocation.data
+            invertLabel = form.InvertLabel.data
 
             if len(systemgroup) > 0:
                 if not invertSystemgroup:
@@ -46,6 +48,11 @@ def report_hotkeys_enabled():
                     host_filter.append(Host.Location.ilike("%" + location + "%"))
                 else:
                     host_filter.append(Host.Location.notilike("%" + location + "%"))
+            if len(label) > 0:
+                if not invertLabel:
+                    host_filter.append(Host.Label.ilike("%"+label+"%"))
+                else:
+                    host_filter.append(Host.Label.notilike("%"+label+"%"))
 
             if 'excel' in request.form:
                 hotkeys = find_hotkeys_enabled_list(host_filter=host_filter)
