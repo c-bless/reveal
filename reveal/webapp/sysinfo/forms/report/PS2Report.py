c4014fe762829908e839e38b95ff17be4f805959
Christoph Bless <github@cbless.de>
2023-12-31 00:38:59 +0100

    #63 label field added as search field added for powershell reports

--- reveal/webapp/sysinfo/forms/report/PS2Report.py
+++ reveal/webapp/sysinfo/forms/report/PS2Report.py
@@ -1,17 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField, SelectField
-from wtforms.validators import Regexp, Optional
+from wtforms.validators import Regexp
 
 from reveal.core.regex import RE_SYSINFO_SYSTEMGROUP
 from reveal.core.regex import RE_SYSINFO_LOCATION
+from reveal.core.regex import RE_SYSINFO_LABEL
 
 
 class PS2ReportForm(FlaskForm):
     SystemGroup = StringField('SystemGroup', validators=[Regexp(regex=RE_SYSINFO_SYSTEMGROUP, message="Invalid input")] )
     Location = StringField('Location', validators=[Regexp(regex=RE_SYSINFO_LOCATION, message="Invalid input")] )
+    Label = StringField('Label', validators=[Regexp(regex=RE_SYSINFO_LABEL, message="Invalid input")])
 
     InvertSystemGroup = BooleanField('Invert SystemGroup')
     InvertLocation = BooleanField('Invert Location')
+    InvertLabel = BooleanField('Invert Label')
 
     search = SubmitField('Search')
 
